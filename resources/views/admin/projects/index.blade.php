f98c5c561f2fcb2ec500f8b3597c38a49a6e8870
DavideAlovisio13 <alovisiod@gmail.com>
2024-05-31 17:06:58 +0200

    (index.blade) add template main

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,28 +1,107 @@
 @extends('layouts.admin')
 
 @section('content')
-{{-- <div class="container">
-    <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">{{__('ID')}}</th>
-            <th scope="col">{{__('Name')}}</th>
-            <th scope="col">{{__('Title')}}</th>
-            <th scope="col">{{__('Description')}}</th>
-            <th scope="col">{{__('Create at')}}</th>
-          </tr>
-        </thead>
-        <tbody>
-            @foreach ($projects as $project)
-            <tr>
-                <th scope="row">{{{ $project->id }}}</th>
-                <td>{{{ $project->name }}}</td>
-                <td>{{{ $project->title }}}</td>
-                <td>{{{ $project->description }}}</td>
-                <td>{{{ $project->created_at }}}</td>
-            </tr>
-            @endforeach
-        </tbody>
-      </table>
-</div> --}}
+    <main>
+        <h1>Analytics</h1>
+        <!-- Analyses -->
+        <div class="analyse">
+            <div class="sales">
+                <div class="status">
+                    <div class="info">
+                        <h3>Total Sales</h3>
+                        <h1>$65,024</h1>
+                    </div>
+                    <div class="progresss">
+                        <svg>
+                            <circle cx="38" cy="38" r="36"></circle>
+                        </svg>
+                        <div class="percentage">
+                            <p>+81%</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="visits">
+                <div class="status">
+                    <div class="info">
+                        <h3>Site Visit</h3>
+                        <h1>24,981</h1>
+                    </div>
+                    <div class="progresss">
+                        <svg>
+                            <circle cx="38" cy="38" r="36"></circle>
+                        </svg>
+                        <div class="percentage">
+                            <p>-48%</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="searches">
+                <div class="status">
+                    <div class="info">
+                        <h3>Searches</h3>
+                        <h1>14,147</h1>
+                    </div>
+                    <div class="progresss">
+                        <svg>
+                            <circle cx="38" cy="38" r="36"></circle>
+                        </svg>
+                        <div class="percentage">
+                            <p>+21%</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End of Analyses -->
+
+        <!-- New Users Section -->
+        <div class="new-users">
+            <h2>New Users</h2>
+            <div class="user-list">
+                <div class="user">
+                    <img src="images/profile-2.jpg">
+                    <h2>Jack</h2>
+                    <p>54 Min Ago</p>
+                </div>
+                <div class="user">
+                    <img src="images/profile-3.jpg">
+                    <h2>Amir</h2>
+                    <p>3 Hours Ago</p>
+                </div>
+                <div class="user">
+                    <img src="images/profile-4.jpg">
+                    <h2>Ember</h2>
+                    <p>6 Hours Ago</p>
+                </div>
+                <div class="user">
+                    <img src="images/plus.png">
+                    <h2>More</h2>
+                    <p>New User</p>
+                </div>
+            </div>
+        </div>
+        <!-- End of New Users Section -->
+
+        <!-- Recent Orders Table -->
+        <div class="recent-orders">
+            <h2>Recent Orders</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Course Name</th>
+                        <th>Course Number</th>
+                        <th>Payment</th>
+                        <th>Status</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody></tbody>
+            </table>
+            <a href="#">Show All</a>
+        </div>
+        <!-- End of Recent Orders -->
+
+    </main>
 @endsection
