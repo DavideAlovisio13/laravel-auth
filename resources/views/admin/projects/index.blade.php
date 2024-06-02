1089f827ae47f2dbc0a0db158ec66bdacae8cfad
DavideAlovisio13 <alovisiod@gmail.com>
2024-06-02 15:37:07 +0200

    (index.blade) add styles to table and cards

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'Projects')
 
 @section('content')
-    <section id="projects">
+    <section id="projects" class="container-fluid">
         @if (session()->has('message'))
             <div class="alert alert-success">{{ session()->get('message') }}</div>
         @endif
@@ -10,99 +10,87 @@
             <h1>Projects</h1>
             <a href="{{ route('admin.projects.create') }}" class="btn btn-primary">Crea nuovo post</a>
         </div>
-
-        {{-- <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th scope="col">Id</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Title</th>
-                    <th scope="col">Description</th>
-                    <th scope="col">Create at</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($projects as $project)
-                    <tr>
-                        <td>{{ $project->id }}</td>
-                        <td>{{ $project->name }}</td>
-                        <td>{{ $project->title }}</td>
-                        <td>{{ $project->description }}</td>
-                        <td>{{ $project->created_at }}</td>
-                        <td>
-                            <a href="{{ route('admin.projects.show', $project->slug) }}"><i class="fa-solid fa-eye"></i></a>
-                            <a href="{{ route('admin.projects.edit', $project->slug) }}"><i class="fa-solid fa-pen"></i></a>
-                            <form action="{{ route('admin.projects.destroy', $project->slug) }}" method="POST"
-                                class="d-inline-block">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="delete-button border-0 bg-transparent"
-                                    data-item-title="{{ $project->title }}">
-                                    <i class="fa-solid fa-trash"></i>
-                                </button>
-
-                            </form>
-
-
-                        </td>
-                    </tr>
-                @endforeach
-
-
-            </tbody>
-        </table> --}}
-        <div class="container text-center">
-            <div class="row">
-                <div class="col-sm-8">
-                    <div class="card">
-                        <div class="card-body">
-                            <table class="table">
-                                <thead>
+        <div class="row">
+            <div class="col-sm-8">
+                <div class="card">
+                    <div class="card-body">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col" class="text-danger">Id</th>
+                                    <th scope="col" class="text-danger">Name</th>
+                                    <th scope="col" class="text-danger">Title</th>
+                                    <th scope="col" class="text-danger">Description</th>
+                                    <th scope="col" class="text-danger">Create at</th>
+                                </tr>
+                            </thead>
+                            <tbody class="table-group-divider">
+                                @foreach ($projects->take(4) as $project)
                                     <tr>
-                                        <th scope="col">Id</th>
-                                        <th scope="col">Name</th>
-                                        <th scope="col">Title</th>
-                                        <th scope="col">Description</th>
-                                        <th scope="col">Create at</th>
+                                        <td>{{ $project->id }}</td>
+                                        <td>{{ $project->name }}</td>
+                                        <td>{{ $project->title }}</td>
+                                        <td>{{ $project->description }}</td>
+                                        <td>{{ $project->created_at }}</td>
+                                        <td class="d-flex flex-column justify-content-center align-items-center">
+                                            <a href="{{ route('admin.projects.show', $project->slug) }}"><i
+                                                    class="fa-solid text-danger fa-eye"></i></a>
+                                            <a href="{{ route('admin.projects.edit', $project->slug) }}"><i
+                                                    class="fa-solid text-danger fa-pen"></i></a>
+                                            <form action="{{ route('admin.projects.destroy', $project->slug) }}"
+                                                method="POST" class="d-inline-block">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="delete-button border-0 bg-transparent"
+                                                    data-item-title="{{ $project->title }}">
+                                                    <i class="fa-solid text-danger fa-trash"></i>
+                                                </button>
+                                            </form>
+                                        </td>
                                     </tr>
-                                </thead>
-                                <tbody class="table-group-divider">
-                                    @foreach ($projects->take(4) as $project)
-                                        <tr>
-                                            <td>{{ $project->id }}</td>
-                                            <td>{{ $project->name }}</td>
-                                            <td>{{ $project->title }}</td>
-                                            <td>{{ $project->description }}</td>
-                                            <td>{{ $project->created_at }}</td>
-                                            <td>
-                                                <a href="{{ route('admin.projects.show', $project->slug) }}"><i
-                                                        class="fa-solid fa-eye"></i></a>
-                                                <a href="{{ route('admin.projects.edit', $project->slug) }}"><i
-                                                        class="fa-solid fa-pen"></i></a>
-                                                <form action="{{ route('admin.projects.destroy', $project->slug) }}"
-                                                    method="POST" class="d-inline-block">
-                                                    @csrf
-                                                    @method('DELETE')
-                                                    <button type="submit" class="delete-button border-0 bg-transparent"
-                                                        data-item-title="{{ $project->title }}">
-                                                        <i class="fa-solid fa-trash"></i>
-                                                    </button>
-                                                </form>
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="card mb-3" style="max-width: 540px;">
+                    <div class="container text-center d-flex flex-column">
+                        <div class="row">
+                            <div class="col p-2">
+                                <img src="/images/404-det.jpg" class="card-img-top rounded-circle p-1" style="width: 300px height: 300px" alt="logo-profile">
+                            </div>
+                            <div class="col align-items-center justify-content-center d-flex">
+                                <h2>{{ Auth::user()->name }}</h2>
+                            </div>
+                        </div>
+                        <div class="row pt-4 fs-1">
+                            <div class="col social_card my-4 d-flex flex-column justify-content-center align-content-center">
+                                <a href=""><i class="fa-brands fa-github fa-2xl text-danger"></i></a>
+                                <h4 class="pt-3">GitHub</h4>
+                            </div>
+                            <div class="col social_card my-4 d-flex flex-column justify-content-center align-content-center">
+                                <a href=""><i class="fa-brands fa-facebook fa-2xl text-danger"></i></a>
+                                <h4 class="pt-3">Facebook</h4>
+                            </div>
+                            <div class="col social_card my-4 d-flex flex-column justify-content-center align-content-center">
+                                <a href=""><i class="fa-brands fa-instagram fa-2xl text-danger"></i></a>
+                                <h4 class="pt-3">Instagram</h4>
+                            </div>
+                            <div class="col social_card my-4 d-flex flex-column justify-content-center align-content-center">
+                                <a href=""><i class="fa-brands fa-x-twitter fa-2xl text-danger"></i></a>
+                                <h4 class="pt-3">Twitter</h4>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-sm-4">col-sm-4</div>
             </div>
-            <div class="row">
-                <div class="col-sm">col-sm</div>
-                <div class="col-sm">col-sm</div>
-                <div class="col-sm">col-sm</div>
-            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm">col-sm</div>
+            <div class="col-sm">col-sm</div>
+            <div class="col-sm">col-sm</div>
         </div>
     </section>
     @include('partials.modal-delete')
